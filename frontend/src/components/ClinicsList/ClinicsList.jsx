9107b8e80a8b7cdbadf3f666e830e7c269c54f37
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-05-01 17:44:19 -0400

    add dummy data to clinics

--- frontend/src/components/ClinicsList/ClinicsList.jsx
+++ frontend/src/components/ClinicsList/ClinicsList.jsx
@@ -1,5 +1,23 @@
 import ClinicListItem from "./ClinicsListItem";
 
+const DUMMY_CLINICS = [
+  {
+    id : 1,
+    name: "WeFixU Clinic",
+    address: "123 Cool Street, City, Country"
+  },
+  {
+    id : 2,
+    name: "Best MD",
+    address: "42 Douglas Lane, City, Country"
+  },
+  {
+    id : 3,
+    name: "Sneezes R Us",
+    address: "777 Luck Avenue, City, Country"
+  }
+]
+
 const ClinicList = () => {
   return(
     <ul>
