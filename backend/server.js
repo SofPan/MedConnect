40d99ac8fe968d49d9f200bd204593590df35349
Dinara Dvortsova <dinara.dvortsova@gmail.com>
2024-04-30 23:42:59 -0400

    db connection setup

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
+// app.js
+
 const express = require('express');
 const dotenv = require('dotenv');
-const cors = require('cors'); 
+const cors = require('cors');
+const db = require('./src/db/connection');
 
 dotenv.config();
 
@@ -8,12 +11,18 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
-app.use(cors()); 
+app.use(cors());
 
+// Testing frontend connection
 app.get('/api', (req, res) => {
-  res.json({test: "test"})
-})
+  res.json({ test: "test" });
+});
 
+// Testing db connection
+app.get('/api/users', async (req, res) => {
+  db.query('SELECT * FROM users;')
+    .then(data => console.log(data.rows))
+});
 
 // Start the server
 app.listen(PORT, () => {
