27ab0976939094af66035db7c58af1d75965ea0a
matt-james-morgan <matt.james.morgan97@gmail.com>
2024-05-02 22:27:59 -0400

    can render different clinics appointments on calendar

--- frontend/src/components/Scheduling/Calendar.jsx
+++ frontend/src/components/Scheduling/Calendar.jsx
@@ -1,45 +1,19 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState, useCallback, useContext } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import interactionPlugin from "@fullcalendar/interaction"; // needed for dayClick
 import timeGridPlugin from '@fullcalendar/timegrid';
+import { UserSignedIn } from '../../App';
 
 export default function Calendar({ inputValue }) {
-  const [events, setEvents] = useState([{ title: 'Event 1', start: new Date('2024-05-01T10:00:00') }]);
+  
 
+    const { userState } = useContext(UserSignedIn);
+    console.log(userState)
   const handleDateClick = (e) => {
     const newEvent = { title: inputValue, start: e.dateStr };
-    setEvents(prev => [...prev, newEvent]);
+    // setEvents(prev => [...prev, newEvent]);
   }
 
-  const clinicId = '1';
-
-  const initialEvents = async (clinicId) => {
-    try {
-      const response = await fetch(`http://localhost:8080/calendar/${clinicId}`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        }
-      });
-
-      if (!response.ok) {
-        throw new Error('Failed to get appointments');
-      }
-
-      const data = await response.json();
-      const newEvent = { title: "IT WORKED", start: new Date(data.details) };
-      setEvents(prev => [...prev, newEvent]);
-    } catch (error) {
-      console.error('Error:', error);
-      // Handle error
-    }
-  };
-
-  useEffect(() => {
-    initialEvents(clinicId);
-  }, []);
-
-  console.log(events);
 
   function renderEventContent(eventInfo) {
     return (
@@ -57,7 +31,7 @@ export default function Calendar({ inputValue }) {
         plugins={[timeGridPlugin, interactionPlugin]}
         initialView='timeGridWeek'
         weekends={false}
-        events={events}
+        events={userState.events}
         eventContent={renderEventContent}
         dateClick={(e) => handleDateClick(e)}
         slotMinTime={"10:00:00"}
--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -7,7 +7,8 @@ export default function useApplicationData() {
   const initialState = {
     userLoggedIn: false,
     userInfo: {},
-    selectedClinicId: 0
+    selectedClinicId: 0,
+    events:[{ title: 'Event 1', start: new Date('2024-05-01T10:00:00') }]
   }
 
   const [userState, dispatch] = useReducer((userState, action) => {
@@ -20,10 +21,47 @@ export default function useApplicationData() {
         return {...userState, userInfo: action.payload}
     case "USER_STATE_LOGOUT":
         return {...userState, userLoggedIn: action.payload}
+    case "USER_SELECTED_CLINIC":
+        return {...userState, selectedClinicId: action.payload}
+    case "CLINIC_EVENTS":
+        console.log("new event", action);
+        return {...userState, events: action.payload}
       default:
         return userState;
     }
   }, { ...initialState});
+
+  
+  useEffect(() => {
+    const fetchData = async () => {
+        try {
+            const response = await fetch(`http://localhost:8080/calendar/${userState.selectedClinicId}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            });
+
+            if (!response.ok) {
+                throw new Error('Failed to get appointments');
+            }
+
+            const data = await response.json();
+            const newEvent = { title: data.patient_id, start: new Date(data.details) };
+
+            dispatch({ type: "CLINIC_EVENTS", payload: [newEvent]});
+
+        } catch (error) {
+            console.error('Error:', error);
+            // Handle error
+        }
+    };
+
+    if (userState.selectedClinicId) {
+        fetchData(); // Call the asynchronous function only if selectedClinicId is truthy
+    }
+
+}, [userState.selectedClinicId]);
 
 
   //fetches photo info related to specific topic from backend
