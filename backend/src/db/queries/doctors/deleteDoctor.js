9065e7b928c7856c03967f1c14072fa6e50e5ced
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-05-04 17:41:35 -0400

    create query to delete doctor from table

--- backend/src/db/queries/doctors/deleteDoctor.js
+++ backend/src/db/queries/doctors/deleteDoctor.js
@@ -1,1 +1,18 @@
 // Delete existing doctor
+const db = require('../../connection');
+
+const deleteDoctor = (id) => {
+  const value = [id]
+  const query = `
+    DELETE FROM doctors
+    WHERE id = $1;
+  `;
+
+  return db.query(query, value)
+    .then(data => {
+      return data.rows[0];
+    })
+    .catch(err => console.error('addNewDoctor query error:', error));
+}
+
+module.exports = { deleteDoctor }
