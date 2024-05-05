4c61949cb7c2be8806d3a2c6b9561e07e216a5ab
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-05-04 21:37:30 -0400

    diagnosing editDoctor query

--- backend/src/db/queries/doctors/editDoctor.js
+++ backend/src/db/queries/doctors/editDoctor.js
@@ -1,1 +1,28 @@
 // Edit existing doctor
+const db = require('../../connection');
+
+const editDoctor = (doctor) => {
+  const value = [
+    doctor.id, // $1
+    doctor.name, //$2
+    doctor.qualifications, // $3
+    doctor.description, // $4
+    doctor.photo_url, // $5
+    doctor.number_of_patients // $6
+  ]
+  console.log("value!", value);
+  const query = `
+    UPDATE doctors
+    SET name = $2, qualifications = $3, description = $4, photo_url = $5, number_of_patients = $6
+    WHERE doctors.id = $1;
+  `;
+
+  return db.query(query, value)
+    .then(data => {
+      console.log("editDoctor data", data.rows[0]);
+      return data.rows[0];
+    })
+    .catch(error => console.error('editDoctor query error:', error));
+}
+
+module.exports = { editDoctor }
