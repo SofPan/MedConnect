b2c89dc51fb5124fc42e7a77b4dc44bd62a4e818
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-05-04 21:13:37 -0400

    handle form submit for editing doctors

--- frontend/src/components/DoctorsList/EditDoctor.jsx
+++ frontend/src/components/DoctorsList/EditDoctor.jsx
@@ -1,19 +1,35 @@
 import { useEffect, useState } from 'react';
-import {
-  Input,
-  InputLabel,
-  Button
-} from '@mui/material';
+import DoctorForm from '../Forms/DoctorForm';
+import { putDoctor } from '../../hooks/tempUseAPI';
 
 const EditDoctorForm = (props) => {
-  const {id} = props;
+  const {doctor} = props;
 
+  const [editDoctor, setEditDoctor] = useState(doctor);
   
-  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const target = e.target.elements;
+    setEditDoctor(() => ({
+      name: target.name.value && target.name.value,
+      qualifications: target.qualifications.value && target.qualifications.value,
+      description: target.description.value && target.description.value,
+      number_of_patients: target.num_patients.value
+    }));
+  }
+
+  useEffect(() => {
+    const editDoctorRequest = async () => {
+      await putDoctor(doctor.id);
+    }
+
+    editDoctorRequest();
+  }, [editDoctor])
+
   return(
-    <form>
-
-    </form>
+    <>
+      <DoctorForm handleSubmit={handleSubmit} />
+    </>
   )
 }
 
