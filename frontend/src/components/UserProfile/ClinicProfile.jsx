a13a87be82e3dc06b1ad757151f76ceb1b8e87e2
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-05-05 20:32:04 -0400

    set up MUI boxes for profile layout

--- frontend/src/components/UserProfile/ClinicProfile.jsx
+++ frontend/src/components/UserProfile/ClinicProfile.jsx
@@ -35,44 +35,46 @@ const ClinicProfile = (props) => {
 
   return(
     <div>
-      <Box height="100vh" width="20vw" display={'inline-block'} borderRight={'1px solid lightgrey'}>
+      <Box width="20vw" display={'inline-block'} borderRight={'1px solid lightgrey'}>
         <Tabs orientation='vertical'>
           <Tab label="Clinic Profile" />
           <Tab label="Scheduling" />
         </Tabs>
       </Box>
-      <article className="profile-main">
-        <div className="profile-left">
-          <div className='profile-information'>
-            <h2>Information</h2>
-            <div>
-              <p>{userProfile.name}</p>
-              <p>{userProfile.address && userProfile.address}</p>
+      <Box width="60vw" display={'inline-block'}>
+        <article className="profile-main">
+          <Box className="profile-left" width="30%" display={'inline-block'}>
+            <div className='profile-information'>
+              <h2>Information</h2>
+              <div>
+                <p>{userProfile.name}</p>
+                <p>{userProfile.address && userProfile.address}</p>
+              </div>
+              {/* <Button>Edit</Button> */}
             </div>
-            {/* <Button>Edit</Button> */}
-          </div>
 
-          <div className='profile-notifications'>
-            <h2>Notifications</h2>
-            <ul>
-              <li>Patient XYZ has requested an appointment</li>
-              <li>Patient ABC wants to change doctors</li>
-            </ul>
-          </div>
-        </div>
-        <div className="profile-right">
-          <div>
-            <h2>Doctors</h2>
-            <Button onClick={handleClick}>New</Button>
-              <NewDoctorForm 
-                clinic_id={userProfile.id}
-                addDoctor={triggerDoctorStateUpdate}
-              />
-          </div>
-            {!doctors.length && <span>You do not have any doctors listed</span>}
-            <DoctorsList clinic_id={userProfile.id} doctors={doctors} changeDoctorState={triggerDoctorStateUpdate} />
-        </div>
-      </article>
+            <div className='profile-notifications'>
+              <h2>Notifications</h2>
+              <ul>
+                <li>Patient XYZ has requested an appointment</li>
+                <li>Patient ABC wants to change doctors</li>
+              </ul>
+            </div>
+          </Box>
+          <Box className="profile-right" width="60%" display={'inline-block'}>
+            <div>
+              <h2>Doctors</h2>
+              <Button onClick={handleClick}>New</Button>
+                <NewDoctorForm 
+                  clinic_id={userProfile.id}
+                  addDoctor={triggerDoctorStateUpdate}
+                />
+            </div>
+              {!doctors.length && <span>You do not have any doctors listed</span>}
+              <DoctorsList clinic_id={userProfile.id} doctors={doctors} changeDoctorState={triggerDoctorStateUpdate} />
+          </Box>
+        </article>
+      </Box>
     </div>
   )
 }
