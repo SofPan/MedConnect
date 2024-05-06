16fe4f4ee1c16ecb1784354ebf775121565dc74d
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-05-05 21:56:56 -0400

    include date of birth when profile belongs to a patient

--- frontend/src/components/UserProfile/PatientProfile.jsx
+++ frontend/src/components/UserProfile/PatientProfile.jsx
@@ -2,7 +2,6 @@ import UserInformation from "./UserInformation";
 
 const PatientProfile = (props) => {
   const {userProfile} = props;
-  console.log(userProfile);
   return(
     <div className="patient-profile">
       <UserInformation userProfile={userProfile} />
--- frontend/src/components/UserProfile/UserInformation.jsx
+++ frontend/src/components/UserProfile/UserInformation.jsx
@@ -1,11 +1,21 @@
 const UserInformation = (props) => {
   const {userProfile} = props;
+
+  const formatBirthDate = (date) => {
+    return date.split("T").shift();
+  }
   return(
     <div className='profile-information'>
       <h2>Information</h2>
       <div>
-        <p>{userProfile.name}</p>
-        <p>{userProfile.address && userProfile.address}</p>
+        <p>Name: {userProfile.name}</p>
+        <p>{userProfile.address && `Address: ${userProfile.address}`}</p>
+        <p> 
+          {
+            userProfile.date_of_birth && 
+            `Date of Birth: ${formatBirthDate(userProfile.date_of_birth)}`
+          }
+          </p>
       </div>
       {/* <Button>Edit</Button> */}
     </div>
