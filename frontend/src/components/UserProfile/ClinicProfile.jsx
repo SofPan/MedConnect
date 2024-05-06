6dfd3659935f351fdba9dd410131a5c4e7e89312
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-05-05 21:27:45 -0400

    fix tabs error MUI

--- frontend/src/components/UserProfile/ClinicProfile.jsx
+++ frontend/src/components/UserProfile/ClinicProfile.jsx
@@ -30,11 +30,11 @@ const ClinicProfile = (props) => {
 
   return(
     <div className="clinic-profile">
-      <Box width="20vw" display={'inline-block'} borderRight={'1px solid lightgrey'}>
-        {/* <Tabs orientation='vertical'>
-          <Tab label="Clinic Profile" />
-          <Tab label="Scheduling" />
-        </Tabs> */}
+      <Box marginRight={"50px"} width="20vw" display={'inline-block'} borderRight={'1px solid lightgrey'}>
+        <Tabs value={0} orientation='vertical'>
+          <Tab label="Clinic Profile" tabIndex={0} value={0}/>
+          <Tab label="Scheduling" tabIndex={1} value={1}/>
+        </Tabs>
       </Box>
       <Box width="60vw" display={'inline-block'}>
         <article className="profile-main">
