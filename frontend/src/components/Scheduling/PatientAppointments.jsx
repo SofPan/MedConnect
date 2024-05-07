520a8d7ad784e78f50c01756dfd18ded5bd52f2b
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-05-07 14:50:47 -0400

    create query to get all appointments related to a patient

--- frontend/src/components/Scheduling/PatientAppointments.jsx
+++ frontend/src/components/Scheduling/PatientAppointments.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import AccordionWrapper from "../GeneralComponents/AccordionWrapper";
 import AppointmentsList from "../AppointmentsList/AppointmentsList";
 
+// Appointments that have been booked by a Patient
 const DUMMY_APPOINTMENTS = [
   {
     id: 1,
@@ -37,17 +38,40 @@ const DUMMY_APPOINTMENTS = [
   }
 ];
 
+// Appointments that have not been claimed
+const DUMMY_OPEN_APPOINTMENTS = [
+  {
+    id: 1,
+    patient_id: null,
+    doctor_id: 1,
+    details: '2024-05-10 09:00:00',
+    clinic_id: 1,
+    status: false
+  },
+  {
+    id: 2,
+    patient_id: null,
+    doctor_id: 1,
+    details: '2024-05-16 09:00:00',
+    clinic_id: 1,
+    status: false
+  },
+]
+
 const PatientAppointments = (props) => {
   const [appointments, setAppointments] = useState(DUMMY_APPOINTMENTS);
 
+  const mapOpenAppointments = DUMMY_OPEN_APPOINTMENTS.map(appointment => {
+    return <li key={appointment.id}>{appointment.details}</li>
+  });
 
+  
   return(
     <>
-    <AccordionWrapper title="Request">
-        {/* <NewDoctorForm 
-          clinic_id={userProfile.id}
-          addDoctor={triggerDoctorStateUpdate}
-        /> */}
+      <AccordionWrapper title="Request">
+        <ul>
+          {mapOpenAppointments}
+        </ul>
       </AccordionWrapper>                
       {!appointments.length && <span>You do not have any appointments booked</span>}
       <AppointmentsList patient_id={1} appointments={appointments}/>
