2af1dcc6fa2ec127fad2b9702893f40abeda2d02
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-05-07 12:00:02 -0400

    create addNewDocument query

--- backend/src/db/queries/documents/addNewDocument.js
+++ backend/src/db/queries/documents/addNewDocument.js
@@ -1,1 +1,23 @@
 // Add a new document
+const db = require('../../connection');
+
+const addNewDocument = (document) => {
+  const value = [
+    document.patient_id, // $1
+    document.document_name, // $2
+    document.document_url, // $3
+  ]
+  const query = `
+    INSERT INTO documents (patient_id, document_name, document_url, created_at) VALUES
+    ($1, $2, $3, NOW())
+    RETURNING *;
+  `;
+
+  return db.query(query, value)
+    .then(data => {
+      return data.rows[0];
+    })
+    .catch(error => console.error('addNewDocument query error:', error));
+}
+
+module.exports = { addNewDocument }
