cdceaf4d51a7cae5a97a7798b7ed2f8de960d5a6
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-05-07 10:18:46 -0400

    create query to delete document from database

--- backend/src/db/queries/documents/deleteDocument.js
+++ backend/src/db/queries/documents/deleteDocument.js
@@ -1,1 +1,19 @@
 // Delete a document
+// Delete existing doctor
+const db = require('../../connection');
+
+const deleteDocument = (id) => {
+  const value = [id]
+  const query = `
+    DELETE FROM documents
+    WHERE id = $1;
+  `;
+
+  return db.query(query, value)
+    .then(data => {
+      return data.rows[0];
+    })
+    .catch(error => console.error('deleteDocument query error:', error));
+}
+
+module.exports = { deleteDocument }
