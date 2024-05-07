b37f07c63d685fd968697006126813c62a073963
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-05-07 10:29:57 -0400

    add hooks and functionality to DocumentsListItem to handle deleting and state updates

--- frontend/src/components/PatientDocuments/DocumentsListItem.jsx
+++ frontend/src/components/PatientDocuments/DocumentsListItem.jsx
@@ -1,17 +1,33 @@
 import DescriptionOutlinedIcon from '@mui/icons-material/DescriptionOutlined';
+import { Button } from '@mui/material';
+import { deleteDocument } from '../../hooks/tempUseAPI';
+import { useEffect, useState } from 'react';
 
 const DocumentsListItem = (props) => {
   const {
     id,
     name,
-    document,
     changeDocumentState
   } = props;
+
+  const [deleting, setDeleting] = useState(false);
+
+  useEffect(() => {
+    const deleteDocumentFromList = async () => {
+      await deleteDocument(id);
+    }
+
+    if(deleting){
+      deleteDocumentFromList();
+      changeDocumentState();
+    }
+  }, [deleting, id, changeDocumentState])
 
   return(
     <li>
       <DescriptionOutlinedIcon/>
       <span>{name}</span>
+      <Button onClick={() => setDeleting(true)}>Delete</Button>
     </li>
   )
 }
