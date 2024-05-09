e129a3fe3b8daefcb7a9f207216624ad74813151
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-05-09 14:28:56 -0400

    edit appointments query

--- backend/src/db/queries/appointments/getAllAppointmentsByPatient.js
+++ backend/src/db/queries/appointments/getAllAppointmentsByPatient.js
@@ -3,8 +3,9 @@ const db = require('../../connection');
 
 const getAllAppointmentsByPatient = (patientId) => {
   const value = [patientId]
+  // appointments.id, patient_id, appointments.start_time, appointments.end_time, appointments.status
   const query = `
-    SELECT appointments.id, patient_id, appointments.details, appointments.status, doctors.name as doctor_name, clinics.address as clinic_address
+    SELECT appointments.*, doctors.name as doctor_name, clinics.address as clinic_address
     FROM appointments
     JOIN doctors ON appointments.doctor_id = doctors.id
     JOIN clinics ON appointments.clinic_id = clinics.id
