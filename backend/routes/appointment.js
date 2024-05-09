9ab77cc3f47a5dfde970e467cde2ad0bf284fbc1
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-05-09 15:07:30 -0400

    create GET route for open appointments

--- backend/routes/appointment.js
+++ backend/routes/appointment.js
@@ -11,30 +11,31 @@ const router = express.Router();
 
 const { getAllAppointmentsByPatient } = require('../src/db/queries/appointments/getAllAppointmentsByPatient');
 const { editAppointment } = require('../src/db/queries/appointments/editAppointment');
-const { getCalendarByClinicId} = require("../src/db/queries/calendar/getCalendarByClinicId");
+const { getCalendarByClinicId } = require("../src/db/queries/calendar/getCalendarByClinicId");
+const { getClinicsOpenAppointments } = require('../src/db/queries/appointments/getClinicsOpenAppointments');
 
 router.get('/:id', (req, res) => {
-  
+
   const clinicId = req.params.id;
-  
+
   getCalendarByClinicId(clinicId)
-      .then(calendar => {
+    .then(calendar => {
 
-        if (!calendar) {
-            // If no calendar is found for the given clinic ID, return a 404 response
-            return res.status(404).json({ error: 'Calendar not found' });
-          }
-          
-          // If calendar is found, return it as JSON response
-          
-          res.json(calendar);
-      })
-      .catch(error => {
-        console.error('Error fetching user information:', error);
-        res.status(500).json({ error: 'Internal server error' });
-      });
+      if (!calendar) {
+        // If no calendar is found for the given clinic ID, return a 404 response
+        return res.status(404).json({ error: 'Calendar not found' });
+      }
 
-  });
+      // If calendar is found, return it as JSON response
+
+      res.json(calendar);
+    })
+    .catch(error => {
+      console.error('Error fetching user information:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    });
+
+});
 
 // Get appointments by patient id
 router.get("/patients/:id", (req, res) => {
@@ -46,6 +47,17 @@ router.get("/patients/:id", (req, res) => {
       res.status(500).json({ error: 'Internal server error' });
     });
 });
+
+// Get open appointments by clinic id
+router.get("/open/:id", (req, res) => {
+  const clinicId = req.params.id;
+  getClinicsOpenAppointments(clinicId)
+    .then(appointmentData => res.json(appointmentData))
+    .catch(error => {
+      console.error("Error fetching patient's appointments: ", error);
+      res.status(500).json({ error: 'Internal server error' });
+    });
+})
 
 // PUT edit appointment /appointment/:id
 /*
