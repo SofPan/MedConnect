87d491a10b03bd037164e63d78e8b0e26cff788a
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-05-09 12:06:28 -0400

    move markup for Notifications into new component

--- frontend/src/components/Notifications/Notifications.jsx
+++ frontend/src/components/Notifications/Notifications.jsx
@@ -1,8 +1,13 @@
 const Notifications = (props) => {
 
   return(
-    <>
-    </>
+    <div className='profile-notifications'>
+      <h2>Notifications</h2>
+      <ul>
+        <li>Patient XYZ has requested an appointment</li>
+        <li>Patient ABC wants to change doctors</li>
+      </ul>
+    </div>
   )
 }
 
