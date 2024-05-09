f31e8060ad2974db7810dca72735035ea6300e08
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-05-09 15:24:05 -0400

    return all data rows

--- backend/src/db/queries/appointments/getClinicsOpenAppointments.js
+++ backend/src/db/queries/appointments/getClinicsOpenAppointments.js
@@ -2,18 +2,19 @@
 const db = require('../../connection');
 
 const getClinicsOpenAppointments = (clinic_id) => {
+  console.log("query reached")
   const values = [clinic_id];
 
   const query = `
     SELECT *
     FROM appointments
     WHERE clinic_id = $1
-    AND patient_id = null;
+    AND patient_id IS NULL;
   `
 
   return db.query(query, values)
     .then(data => {
-      return data.rows[0];
+      return data.rows;
     })
     .catch(error => console.error('getClinicsOpenAppointments query error:', error));
 }
