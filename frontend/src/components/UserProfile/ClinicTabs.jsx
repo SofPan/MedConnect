a13fcaf71d7a7b5a49fb85a416b4363d6649689c
Dinara Dvortsova <dinara.dvortsova@gmail.com>
2024-05-10 14:51:20 -0400

    Implement swiitching tabs

--- frontend/src/components/UserProfile/ClinicTabs.jsx
+++ frontend/src/components/UserProfile/ClinicTabs.jsx
@@ -1,15 +1,21 @@
 import {
   Tabs,
   Tab,
-  Box
-} from '@mui/material';
+  Box,
+  } from '@mui/material';
+
 
 const ClinicTabs = (props) => {
+  const {activeTab, handleTabChange} = props;
+
+
+  console.log(activeTab)
+  
   return (
     <Box marginRight={"50px"} width="20vw" display={'inline-block'} borderRight={'1px solid lightgrey'}>
-      <Tabs value={0} orientation='vertical'>
-        <Tab label="Clinic Profile" tabIndex={0} value={0}/>
-        <Tab label="Scheduling" tabIndex={1} value={1}/>
+      <Tabs value={activeTab} onChange={handleTabChange} orientation='vertical'>
+        <Tab label="Clinic Profile"/>
+        <Tab label="Scheduling"/>
       </Tabs>
     </Box>
   )
---
+++ frontend/src/components/UserProfile/TabContent.jsx
@@ -0,0 +1,18 @@
+function TabContent (props) {
+  const { children, value, index } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+    >
+      {value === index && (
+        <div>{children}</div>
+      )}
+    </div>
+  );
+}
+
+export default TabContent;
