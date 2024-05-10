3d38d44604e2e238f096d9f128b54aa79e85ea15
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-05-10 11:39:45 -0400

    create GET request route for pending notifications

--- backend/routes/request.js
+++ backend/routes/request.js
@@ -1,33 +1,44 @@
 const express = require('express');
 const { addRequest } = require('../src/db/queries/clinics/addRequest');
 const { getRequest } = require('../src/db/queries/clinics/getRequestByData');
-const router  = express.Router();
+const { getRequestByClinic } = require('../src/db/queries/clinics/getRequestByClinic');
+const router = express.Router();
 
-  router.post('/', (req, res) => {
-    const requestData = {
-      request_type: req.body.request_type,
-      patient_id: req.body.patient_id,
-      clinic_id: req.body.clinic_id,
-      doctor_id: req.body.doctor_id,
-      appointment_id: req.body.appointment_id
-    }
+router.get('/:id', (req, res) => {
+  const clinicId = req.params.id;
+  getRequestByClinic(clinicId)
+    .then(response => res.json(response))
+    .catch(error => {
+      console.error('Error fetching requests:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    })
+});
 
-    getRequest(requestData.request_type, requestData.patient_id)
-      .then(request => {
-        if(request) {
-          res.json({message: "The request has already been sent"});
-        } else {
-          addRequest(requestData)
+router.post('/', (req, res) => {
+  const requestData = {
+    request_type: req.body.request_type,
+    patient_id: req.body.patient_id,
+    clinic_id: req.body.clinic_id,
+    doctor_id: req.body.doctor_id,
+    appointment_id: req.body.appointment_id
+  }
+
+  getRequest(requestData.request_type, requestData.patient_id)
+    .then(request => {
+      if (request) {
+        res.json({ message: "The request has already been sent" });
+      } else {
+        addRequest(requestData)
           .then(request => {
             res.json(request);
           })
-        }
-      })
+      }
+    })
     .catch(error => {
       console.error('Error adding request:', error);
       res.status(500).json({ error: 'Internal server error' });
     });
-  });
+});
 
 
 
