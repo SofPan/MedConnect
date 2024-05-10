5949abc0ec63dee611c883e841f4e7fde76c8728
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-05-10 12:01:03 -0400

    prepare accept and decline buttons for notifications

---
+++ frontend/src/components/Notifications/NotificationActions.jsx
@@ -0,0 +1,15 @@
+
+import {Button} from "@mui/material"
+
+const NotificationActions = (props) => {
+
+  return(
+    <div>
+      <Button>Accept</Button>
+      <Button>Decline</Button>
+    </div>
+  )
+}
+
+export default NotificationActions;
+
