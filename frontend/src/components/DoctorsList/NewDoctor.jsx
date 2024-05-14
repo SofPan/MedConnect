de667cc5c1b93d2d5fabf4abec169f1730a50ea5
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-05-14 10:59:35 -0400

    use context when adding a new doctor

--- frontend/src/components/DoctorsList/NewDoctor.jsx
+++ frontend/src/components/DoctorsList/NewDoctor.jsx
@@ -1,10 +1,12 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { usePost } from '../../hooks/useAPI';
 import DoctorForm from '../Forms/DoctorForm';
+import { UserSignedIn } from '../../App';
 
 const NewDoctorForm = (props) => {
   const {clinic_id} = props;
   const [doctor, setDoctor] = useState({});
+  const {dispatch} = useContext(UserSignedIn);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -22,11 +24,13 @@ const NewDoctorForm = (props) => {
   const {post} = usePost();
 
   useEffect(() => {
-    doctor.name && 
+    if(doctor.name){
       post(
         'doctors',
         doctor
       );
+      dispatch({type: "ADD_DOCTOR", payload: doctor});
+    } 
   }, [doctor]);
 
 
