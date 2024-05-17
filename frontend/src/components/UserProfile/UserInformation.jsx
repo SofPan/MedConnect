bdc4ef0452f01c85b6c467473a4633b09c19317e
Dinara Dvortsova <dinara.dvortsova@gmail.com>
2024-05-16 20:30:07 -0400

    Display doctor in patient profile

--- frontend/src/components/UserProfile/UserInformation.jsx
+++ frontend/src/components/UserProfile/UserInformation.jsx
@@ -1,8 +1,16 @@
+import { useContext } from "react";
+import { UserSignedIn } from "../../App";
+
 const UserInformation = (props) => {
   const {userProfile} = props;
   const formatBirthDate = (date) => {
     return date.split("T").shift();
   }
+
+  const { userState } = useContext(UserSignedIn);
+
+  const doctor = userState.doctors.find(doctor => doctor.id === userProfile.doctor_id)
+
   return(
     <div className='profile-information'>
       <h2>Information</h2>
@@ -13,6 +21,18 @@ const UserInformation = (props) => {
           {
             userProfile.date_of_birth && 
             `Date of Birth: ${formatBirthDate(userProfile.date_of_birth)}`
+          }
+        </p>
+        <p> 
+          {
+            userProfile.doctor_id && 
+            `Doctor: ${doctor.name}`
+          }
+          </p>
+          <p> 
+          {
+            !userProfile.is_clinic && !userProfile.doctor_id && 
+            `You are not registered with a doctor`
           }
           </p>
       </div>
